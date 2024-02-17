03d4161494a53bb3b810592e833f64aee4fe05b7
New Author Name <739856334zhou@gmail.com>
2024-02-17 20:26:20 +0800

    widget

---
+++ StatsCardView/StatsCardViewBundle.swift
@@ -0,0 +1,16 @@
+//
+//  StatsCardViewBundle.swift
+//  StatsCardView
+//
+//  Created by zhouwen on 2024/2/17.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct StatsCardViewBundle: WidgetBundle {
+    var body: some Widget {
+        StatsCardView()
+    }
+}
